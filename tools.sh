25859cb63e5a4e721f218a61ddf6e4f40d71ee62
YYDS <125422525+MR-YYDS@users.noreply.github.com>
2025-03-14 22:56:22 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -4204,7 +4204,6 @@ export PYENV_ROOT="\$HOME/.pyenv"
 if [[ -d "\$PYENV_ROOT/bin" ]]; then
   export PATH="\$PYENV_ROOT/bin:\$PATH"
 fi
-eval "\$(pyenv init --path)"
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 
